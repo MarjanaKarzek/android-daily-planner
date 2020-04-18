61a674299cce7f866f609c05f0ee2184bc6d5613
MarjanaKarzek <marjana.karzek@outlook.com>
2020-04-18 14:36:09 +0200

    [#01] setup project structure

---
+++ app/src/main/java/com/karzek/planner/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.karzek.planner
+
+import android.os.Bundle
+import com.karzek.core.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ buildSrc/src/main/java/com/karzek/buildsrc/dependencies.kt
@@ -0,0 +1,39 @@
+package com.karzek.buildsrc
+
+object Config {
+    const val minSdk = 24
+    const val targetSdk = 29
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val buildToolsVersion = "29.0.2"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:3.6.1"
+
+    object Kotlin {
+        private const val version = "1.3.61"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+    }
+
+    object AndroidX {
+        const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+        const val coreKtx = "androidx.core:core-ktx:1.2.0"
+    }
+
+    object Test {
+        object Mockk {
+            private const val version = "1.9.3"
+            const val mockk = "io.mockk:mockk:${version}"
+            const val mockkAndroid = "io.mockk:mockk-android:${version}"
+        }
+
+        object JUnit {
+            private const val version = "5.6.1"
+            const val jupiterApi = "org.junit.jupiter:junit-jupiter-api:${version}"
+            const val jupiterEngine = "org.junit.jupiter:junit-jupiter-engine:${version}"
+            const val junit5VintageEngine = "org.junit.vintage:junit-vintage-engine:${version}"
+        }
+    }
+}
